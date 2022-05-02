7ea62620d12f8c03d416bf1dd51dac8b7c499a0b
Alexander <al.x.falconer@gmail.com>
2022-05-02 14:00:00 +0100

    done

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,7 +1,20 @@
 function balancingParentheses(string) {
-  // type your code here
-}
+  let opener = 0
+  let closer = 0
 
+  for (const item of string) {
+    item === "(" ? opener +=1 : closer +=1
+  }
+
+  return closer - opener
+
+  // for (const item of string) {
+  //   item === "(" ? opener += 1 : opener -=1
+  // }
+
+  // return Math.abs(opener)
+
+}
 if (require.main === module) {
   // add your own tests in here
   console.log("Expecting: 0");
